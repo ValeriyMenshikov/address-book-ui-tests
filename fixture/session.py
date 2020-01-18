3f2d3b291f6f5b1c0a111be5f17f2ef226404763
Valeriy Menshikov <team095hunter@gmail.com>
2020-01-19 02:36:10 +0600

    Фикстура сделана общей, добавлено ожидание в метод logout

--- fixture/session.py
+++ fixture/session.py
@@ -14,3 +14,5 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+        wd.implicitly_wait(10)
+        wd.find_element_by_name("user")
