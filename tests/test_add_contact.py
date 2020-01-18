bb7136fe0b2585d4b2e61c8aeb5ee56b3a22b649
Valeriy Menshikov <team095hunter@gmail.com>
2020-01-19 01:21:39 +0600

    Выделен класс для работы с контактами

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_new_contact(Contact(firstname="asdasdasd", middlename="asdasd", lastname="asdfvf", nickname="asdfsdf",
-                                   photo="D:\\DocLuber.pdf", title="sadfvsdf", company="asdfsdfsdf",
-                                   address="bvbbgfbdfgb", home="dcfscfsd", mobile="12312323", work="dsfsdf",
-                                   fax="123123123", mail="asdasd@mail.ru", email2="asdsad@mail.ru",
-                                   email3="ghh213@gmail.ru", homepage="asdasd", bday="1",
-                                   bmonth="January", byear="1999", aday="11", amonth="February", ayear="2000",
-                                   address2="sdfsdfsdf", phone2="ghjnhjkn", notes="hgjkhmjkhjkm"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="asdasdasd", middlename="asdasd", lastname="asdfvf", nickname="asdfsdf",
+                               photo="D:\\DocLuber.pdf", title="sadfvsdf", company="asdfsdfsdf",
+                               address="bvbbgfbdfgb", home="dcfscfsd", mobile="12312323", work="dsfsdf",
+                               fax="123123123", mail="asdasd@mail.ru", email2="asdsad@mail.ru",
+                               email3="ghh213@gmail.ru", homepage="asdasd", bday="1",
+                               bmonth="January", byear="1999", aday="11", amonth="February", ayear="2000",
+                               address2="sdfsdfsdf", phone2="ghjnhjkn", notes="hgjkhmjkhjkm"))
+    app.session.logout()
