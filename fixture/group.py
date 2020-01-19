14fb3fbccec056a276fb67e5164ccd2f8696c2e7
Valeriy Menshikov <team095hunter@gmail.com>
2020-01-19 10:48:58 +0600

    Создан метод для удаления первой группы

--- fixture/group.py
+++ fixture/group.py
@@ -22,6 +22,16 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init group creation
+        wd.find_element_by_name("selected[]").click()
+        # fill group form
+        wd.find_element_by_name("delete").click()
+        # submit group creation
+        self.return_to_groups_page()
+
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
