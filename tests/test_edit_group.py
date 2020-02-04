0561957b9814cb606ca914da66549aaaf2b8dcf0
Valeriy Menshikov <team095hunter@gmail.com>
2020-02-04 21:12:37 +0600

    Добавлены проверки на количество групп

--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -4,4 +4,7 @@ from model.group import Group
 def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="1111", header="111", footer="111"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="222", header="333", footer="444"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
