a0f0ec070ce6245c0447e90f8c774ad3b39e5e83
Valeriy Menshikov <team095hunter@gmail.com>
2020-02-09 23:22:17 +0600

    Параметризовал тесты

--- fixture/contact.py
+++ fixture/contact.py
@@ -10,9 +10,7 @@ class ContactHelper:
 
     def open_home_page(self):
         wd = self.app.wd
-        if not ((wd.current_url.endswith("addressbook/")
-                 or wd.current_url.endswith("/index.php"))
-                and len(wd.find_elements_by_name("searchstring")) > 0):
+        if len(wd.find_elements_by_name("searchstring")) < 0:
             wd.find_element_by_link_text("home").click()
 
     def create(self, contact):
--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,15 +1,201 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 
+import pytest
+import random
+import string
 
-def test_add_contact(app):
+
+def random_string(maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + ' ' * 10
+    return ''.join([random.choice(symbols) for _ in range(random.randrange(maxlen))])
+
+
+test_data = [''] + [random_string(20) for _ in range(2)]
+
+
+@pytest.mark.parametrize("field", test_data, ids=[repr(x) for x in test_data])
+def test_add_contact_name(app, field):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="asdasdasd", middlename="asdasd", lastname="asdfvf", nickname="asdfsdf",
-                      title="sadfvsdf", company="asdfsdfsdf", address="bvbbgfbdfgb", homephone="dcfscfsd",
-                      mobilephone="12312323", workphone="dsfsdf", faxphone="123123123", mail="asdasd@mail.ru",
-                      email2="asdsad@mail.ru", email3="ghh213@gmail.ru", homepage="asdasd", bday="1",
-                      bmonth="January", byear="1999", aday="11", amonth="February", ayear="2000",
-                      address2="sdfsdfsdf", secondaryphone="ghjnhjkn", notes="hgjkhmjkhjkm")
+    contact = Contact(firstname=field, middlename='middlename',
+                      lastname='lastname', nickname='nickname',
+                      title='title', company='company',
+                      address='address', homephone=r'+79509999999',
+                      mobilephone=r'+79509999999', workphone=r'+79509999999',
+                      faxphone=r'+79509999999', mail='mail',
+                      email2='email2', email3='email3',
+                      homepage='homepage',
+                      bday="1", bmonth="January", byear="1999",
+                      aday="11", amonth="February", ayear="2000",
+                      address2='address2', secondaryphone='secondaryphone')
+    app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
+    new_contacts = app.contact.get_contact_list()
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
+test_data = [''] + [random_string(12) for _ in range(2)]
+
+
+@pytest.mark.parametrize("field", test_data, ids=[repr(x) for x in test_data])
+def test_add_contact_middlename(app, field):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname='firstname', middlename=field,
+                      lastname='lastname', nickname='nickname',
+                      title='title', company='company',
+                      address='address', homephone=r'+79509999999',
+                      mobilephone=r'+79509999999', workphone=r'+79509999999',
+                      faxphone=r'+79509999999', mail='mail',
+                      email2='email2', email3='email3',
+                      homepage='homepage',
+                      bday="1", bmonth="January", byear="1999",
+                      aday="11", amonth="February", ayear="2000",
+                      address2='address2', secondaryphone='secondaryphone')
+    app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
+    new_contacts = app.contact.get_contact_list()
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
+test_data = [''] + [random_string(12) for _ in range(2)]
+
+
+@pytest.mark.parametrize("field", test_data, ids=[repr(x) for x in test_data])
+def test_add_contact_lastname(app, field):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname='firstname', middlename='middlename',
+                      lastname=field, nickname='nickname',
+                      title='title', company='company',
+                      address='address', homephone=r'+79509999999',
+                      mobilephone=r'+79509999999', workphone=r'+79509999999',
+                      faxphone=r'+79509999999', mail='mail',
+                      email2='email2', email3='email3',
+                      homepage='homepage',
+                      bday="1", bmonth="January", byear="1999",
+                      aday="11", amonth="February", ayear="2000",
+                      address2='address2', secondaryphone='secondaryphone')
+    app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
+    new_contacts = app.contact.get_contact_list()
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
+test_data = [''] + [random_string(12) for _ in range(2)]
+
+
+@pytest.mark.parametrize("field", test_data, ids=[repr(x) for x in test_data])
+def test_add_contact_nickname(app, field):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname='firstname', middlename='middlename',
+                      lastname='lastname', nickname=field,
+                      title='title', company='company',
+                      address='address', homephone=r'+79509999999',
+                      mobilephone=r'+79509999999', workphone=r'+79509999999',
+                      faxphone=r'+79509999999', mail='mail',
+                      email2='email2', email3='email3',
+                      homepage='homepage',
+                      bday="1", bmonth="January", byear="1999",
+                      aday="11", amonth="February", ayear="2000",
+                      address2='address2', secondaryphone='secondaryphone')
+    app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
+    new_contacts = app.contact.get_contact_list()
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
+test_data = [''] + [random_string(12) for _ in range(2)]
+
+
+@pytest.mark.parametrize("field", test_data, ids=[repr(x) for x in test_data])
+def test_add_contact_title(app, field):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname='firstname', middlename='middlename',
+                      lastname='lastname', nickname='nickname',
+                      title=field, company='company',
+                      address='address', homephone=r'+79509999999',
+                      mobilephone=r'+79509999999', workphone=r'+79509999999',
+                      faxphone=r'+79509999999', mail='mail',
+                      email2='email2', email3='email3',
+                      homepage='homepage',
+                      bday="1", bmonth="January", byear="1999",
+                      aday="11", amonth="February", ayear="2000",
+                      address2='address2', secondaryphone='secondaryphone')
+    app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
+    new_contacts = app.contact.get_contact_list()
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
+test_data = [''] + [random_string(12) for _ in range(2)]
+
+
+@pytest.mark.parametrize("field", test_data, ids=[repr(x) for x in test_data])
+def test_add_contact_company(app, field):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname='firstname', middlename='middlename',
+                      lastname='lastname', nickname='nickname',
+                      title='title', company=field,
+                      address='address', homephone=r'+79509999999',
+                      mobilephone=r'+79509999999', workphone=r'+79509999999',
+                      faxphone=r'+79509999999', mail='mail',
+                      email2='email2', email3='email3',
+                      homepage='homepage',
+                      bday="1", bmonth="January", byear="1999",
+                      aday="11", amonth="February", ayear="2000",
+                      address2='address2', secondaryphone='secondaryphone')
+    app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
+    new_contacts = app.contact.get_contact_list()
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
+test_data = [''] + [random_string(12) for _ in range(2)]
+
+
+@pytest.mark.parametrize("field", test_data, ids=[repr(x) for x in test_data])
+def test_add_contact_address(app, field):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname='firstname', middlename='middlename',
+                      lastname='lastname', nickname='nickname',
+                      title='title', company='company',
+                      address=field, homephone=r'+79509999999',
+                      mobilephone=r'+79509999999', workphone=r'+79509999999',
+                      faxphone=r'+79509999999', mail='mail',
+                      email2='email2', email3='email3',
+                      homepage='homepage',
+                      bday="1", bmonth="January", byear="1999",
+                      aday="11", amonth="February", ayear="2000",
+                      address2='address2', secondaryphone='secondaryphone')
+    app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
+    new_contacts = app.contact.get_contact_list()
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
+test_data = [''] + [random_string(12) for _ in range(2)]
+
+
+@pytest.mark.parametrize("field", test_data, ids=[repr(x) for x in test_data])
+def test_add_contact_homephone(app, field):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname='firstname', middlename='middlename',
+                      lastname='lastname', nickname='nickname',
+                      title='title', company='company',
+                      address='address', homephone=field,
+                      mobilephone=r'+79509999999', workphone=r'+79509999999',
+                      faxphone=r'+79509999999', mail='mail',
+                      email2='email2', email3='email3',
+                      homepage='homepage',
+                      bday="1", bmonth="January", byear="1999",
+                      aday="11", amonth="February", ayear="2000",
+                      address2='address2', secondaryphone='secondaryphone')
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
