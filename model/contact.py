bcc6eb1c1adce82737ee05548a843fbcafb0ca21
Valeriy Menshikov <team095hunter@gmail.com>
2020-02-09 18:22:35 +0600

    Реализована обратная проверка для телефонов

--- model/contact.py
+++ model/contact.py
@@ -5,7 +5,8 @@ class Contact:
     def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, photo=None, title=None,
                  company=None, address=None, homephone=None, mobilephone=None, workphone=None, new_group=None,
                  faxphone=None, mail=None, email2=None, email3=None, homepage=None, bday=None, bmonth=None, byear=None,
-                 aday=None, amonth=None, ayear=None, address2=None, secondaryphone=None, notes=None, id=None):
+                 aday=None, amonth=None, ayear=None, address2=None, secondaryphone=None, notes=None, id=None,
+                 all_phones_from_home_page=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -33,6 +34,7 @@ class Contact:
         self.secondaryphone = secondaryphone
         self.notes = notes
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
         return "%s:%s:%s" % (self.firstname, self.lastname, self.id)
