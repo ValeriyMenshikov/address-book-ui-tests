ecf94ee1e58244fe1ab8ce8cc36e35779f6379f4
Valeriy Menshikov <team095hunter@gmail.com>
2020-02-15 10:40:50 +0600

    Реализована динамическая генерация тестов

---
+++ data/groups.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from model.group import Group
+
+test_data = [
+    Group(name="name", header="header", footer="footer"),
+    Group(name="name1", header="header1", footer="footer")
+]
