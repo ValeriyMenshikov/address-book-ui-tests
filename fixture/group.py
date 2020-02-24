38ff691a1b8023fb6e1264fa32dea40ff1214e11
Valeriy Menshikov <team095hunter@gmail.com>
2020-02-24 21:56:28 +0600

    Тест переведен на получение списка из БД

--- fixture/group.py
+++ fixture/group.py
@@ -43,6 +43,15 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        # submit group deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
     def edit_first_group(self, group):
         self.edit_group_by_index(0, group)
 
@@ -67,6 +76,11 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_css_selector(f"input[value='{id}']").click()
 
     def open_groups_page(self):
         wd = self.app.wd
--- tests/test_db_matches_ui.py
+++ tests/test_db_matches_ui.py
@@ -5,7 +5,6 @@ import re
 def test_group_list(app, db):
     def clean(group):
         return Group(id=re.sub(r'\s', '', group.id), name=re.sub(r'\s', '', group.name))
-
     ui_list = map(clean, app.group.get_group_list())
     db_list = map(clean, db.get_group_list())
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
