997d7c586f9154f3f0942f7284da4999da6e073d
Valeriy Menshikov <team095hunter@gmail.com>
2020-03-17 19:07:42 +0600

    Переписал тест добавления групп с использованием Allure

--- data/groups.py
+++ data/groups.py
@@ -3,5 +3,6 @@ from model.group import Group
 
 test_data = [
     Group(name="name", header="header", footer="footer"),
-    Group(name="name1", header="header1", footer="footer")
+    Group(name="name1", header="header1", footer="footer"),
+    Group(name="name1'", header="header1", footer="footer")
 ]
